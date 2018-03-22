0ea04e486c30f3c37b2dcec58a2da35ae83001a6
michael emmons <memmons18@gmail.com>
2018-03-22 10:59:52 -0400

    set up API and is now finding movies on search and adding favorites to database

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -8,4 +8,11 @@ module.exports= function(app){
             res.json(results);
         })
     })
+
+    app.post("/api/addmovie",function(req,res){
+        connection.query("INSERT INTO movie_info (movie_title, genre, pic_id, director, actors, plot, boxOffice, imdb_rating, rt_rating) VALUES (? , ? , ? , ? , ? , ? , ? , ? , ?)",[req.body.movie_title, req.body.genre, req.body.pic_id, req.body.director, req.body.actors, req.body.plot, req.body.boxOffice, req.body.imdb_rating, req.body.rt_rating], function(err,results){
+           console.log("in")
+            res.end();
+        })
+    })
 }
